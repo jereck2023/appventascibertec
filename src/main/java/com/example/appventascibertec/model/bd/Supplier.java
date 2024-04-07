0e3ebfd1fd6a00277bac4b94e6a1f3e692a181ab
jereck2023 <jereck2020@gmail.com>
2024-04-07 10:08:30 -0500

    primera parte de mapeo de objeto BD

--- src/main/java/com/example/appventascibertec/model/bd/Supplier.java
+++ src/main/java/com/example/appventascibertec/model/bd/Supplier.java
@@ -1,4 +1,39 @@
 package com.example.appventascibertec.model.bd;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="suppliers")
 public class Supplier {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer supplierid;
+    @Column(name="companyname")
+    private String companyname;
+    @Column(name="contactname")
+    private String contactname;
+    @Column(name="contacttitle")
+    private String contacttitle;
+    @Column(name="adrees")
+    private String adrees;
+    @Column(name="city")
+    private String city;
+    @Column(name="region")
+    private String region;
+    @Column(name="postalcode")
+    private String postalcode;
+    @Column(name="country")
+    private String country;
+    @Column(name="phone")
+    private String phone;
+    @Column(name="fax")
+    private String fax;
+    @Column(name="homepage")
+    private String homepage;
+
+    public Supplier(String homepage) {
+        this.homepage = homepage;
+    }
 }
